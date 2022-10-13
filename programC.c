59bb656923569f13ca3d37091a2538007e9a9c76
ZeyaTsu <43354103+ZeyaTsu@users.noreply.github.com>
2022-10-13 12:42:14 +0200

    Add files via upload

---
+++ programC.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    int ni = 4;
+    float nf = 3.14;
+    char c;
+
+    c = 'c';
+    ni += ni;
+    nf + 2;
+
+    printf("Variables test");
+
+    printf("The letter is %c\n", c);
+    printf("Ni= %d\n", ni);
+    printf("Nf= %f\n", nf);
+    
+    return 0;
+}
